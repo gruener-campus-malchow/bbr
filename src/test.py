303fcc6ddbfe206e2b8f704ba4434167805632ba
Alexander Baldauf <baldauf@gruener-campus-malchow.de>
2024-12-28 20:49:16 +0100

    tag2.27

--- src/test.py
+++ src/test.py
@@ -1,26 +1,26 @@
 from Motor import Motor
 import time
 
-#motor_L = Motor('MOTOR_LEFT')
-
-#motor_L.set_power(0.5)
-#motor_L.load_config()
-#motor_L.print_config()
+motor_L = Motor('MOTOR_LEFT')
+motor_L.load_config()
+motor_L.print_config()
+motor_L.initiate_servo()
 
 motor_R = Motor('MOTOR_RIGHT')
 motor_R.load_config()
 motor_R.print_config()
-motor_Right = motor_R.initiate_servo()
+motor_R.initiate_servo()
 
 try:
     while True:
         power = input("set power level: ")
         motor_R.set_power(power)
-        print("rotiere weiter für 5 Sekunden")
-        time.sleep(5)
+        motor_L.set_power(power)
+        #print("rotiere weiter für 5 Sekunden")
+        #time.sleep(5)
 
 except KeyboardInterrupt:
-    servo.stop()
+    #servo.stop()
     GPIO.cleanup()
 
 
