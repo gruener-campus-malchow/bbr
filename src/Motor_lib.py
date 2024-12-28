6270a23995652134ef3e6f4674e8d9b97f98506b
Alexander Baldauf <baldauf@gruener-campus-malchow.de>
2024-12-28 15:03:43 +0100

    tag2.22

---
+++ src/Motor_lib.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*- 
+import configparser
+import RPi.GPIO as GPIO
+import time
+
+name = 'CENTER'
+pin = 1
+frequency = 50
+forward_min = 6
+forward_max = 7
+backward_min = 8
+backward_max = 9
+dutyCycle = 0
+steps = 0.05
+servo = True
+	
+def set_power(servo):
+    print("want to set power")
+    foo = servo.ChangeDutyCycle(8.0)
+    print(str(foo))
+    return "did it"
+
+
+def initiate_servo():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(13, GPIO.OUT)
+
+    servo = GPIO.PWM(13, 15) # GPIO 18 als PWM mit 50Hz
+    servo.start(0) # Initialisierung
+    
+    return servo
+    
---
+++ src/testlib.py
@@ -0,0 +1,13 @@
+import Motor_lib
+import time
+
+#motor_L = Motor('MOTOR_LEFT')
+
+#motor_L.set_power(0.5)
+#motor_L.load_config()
+#motor_L.print_config()
+
+
+motor_R = initiate_servo()
+set_power(motor_R)
+        
