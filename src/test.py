0e9d6fbf9617097825bff3082bed3e721afc7636
Alexander Baldauf <baldauf@gruener-campus-malchow.de>
2024-12-28 12:00:28 +0100

    tag2.1

--- src/test.py
+++ src/test.py
@@ -1,13 +1,13 @@
 from Motor import Motor
 import time
 
-motor_L = Motor('LEFT')
+motor_L = Motor('MOTOR_LEFT')
 
 #motor_L.set_power(0.5)
 motor_L.load_config()
 motor_L.print_config()
 
-motor_R = Motor('RIGHT')
+motor_R = Motor('MOTOR_RIGHT')
 motor_R.load_config()
 motor_R.print_config()
 
