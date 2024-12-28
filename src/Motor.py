22cc889e795bcc235c0572d8990f1a817630b870
Alexander Baldauf <baldauf@gruener-campus-malchow.de>
2024-12-28 20:34:32 +0100

    tag2.26

--- src/Motor.py
+++ src/Motor.py
@@ -20,8 +20,21 @@ class Motor():
     	self.name = name
     	pass
     	
-    def set_power(self, servo):
-        print("want to set power")
+    def set_power(self, power):
+        power = int(power)
+        if power > 0:
+            forward_range = abs(forward.fast - forward.slow)
+            forward_steps = forward_range/100
+            dc = power * forward_steps
+        if power <= 0:
+            backward_range = abs(backward.fast - backward.slow)
+            backward_steps = backward_range/100
+            dc = abs(power) * backward_steps
+        print("hhhuuuhh: "+str(dc))
+        if abs(power)>100:
+            print("Wrong power range: use an integer between -100 an 100)
+            return False
+
         foo = self.servo.ChangeDutyCycle(8.0)
         print(str(foo))
         return "did it"
@@ -30,20 +43,20 @@ class Motor():
         config = configparser.ConfigParser()
         config.read('config.ini')
         self.pin = int(config[self.name]['pin'])
-        self.forward_min = float(config[self.name]['forward_min'])
-        self.forward_max = float(config[self.name]['forward_max'])
-        self.backward_min = float(config[self.name]['backward_min'])
-        self.backward_max = float(config[self.name]['backward_max'])
+        self.forward_min = float(config[self.name]['forward_slow'])
+        self.forward_max = float(config[self.name]['forward_fast'])
+        self.backward_min = float(config[self.name]['backward_slow'])
+        self.backward_max = float(config[self.name]['backward_fast'])
         self.frequency = int(config['GENERAL']['frequency'])
     
     def print_config(self):
         print("name of the motor: " + self.name)
         print("pin on the header: " + str(self.pin))
         print("frequency of the pwm-signal: " + str(self.frequency))
-        print("minimal dutycycle for rotating forward: " + str(self.forward_min))
-        print("maximal dutycycle for rotating forward: " + str(self.forward_max))
-        print("minimal dutycycle for rotating backward: " + str(self.backward_min))
-        print("maximal dutycycle for rotating backward: " + str(self.backward_max))
+        print("dutycycle for rotating slow forward: " + str(self.forward_slow))
+        print("dutycycle for rotating fast forward: " + str(self.forward_fast))
+        print("dutycycle for rotating slow backward: " + str(self.backward_slow))
+        print("dutycycle for rotating fast backward: " + str(self.backward_fast))
 
     def initiate_servo(self):
         GPIO.setmode(GPIO.BCM)
@@ -51,8 +64,8 @@ class Motor():
 
         self.servo = GPIO.PWM(self.pin, self.frequency) # GPIO 18 als PWM mit 50Hz
         self.servo.start(0) # Initialisierung
-        self.servo.ChangeDutyCycle(8)
-        return self.servo
+        # self.servo.ChangeDutyCycle(8)
+        # return self.servo
         
 
     def increase(self):
