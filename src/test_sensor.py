42915180a2af7c65887f8437a4a9f0da2ea322a3
Alexander Baldauf <baldauf@gruener-campus-malchow.de>
2024-12-29 20:34:58 +0100

    tag3.4

--- src/test_sensor.py
+++ src/test_sensor.py
@@ -7,17 +7,18 @@ sensor_L.load_config()
 sensor_L.print_config()
 sensor_L.initiate_sensor()
 
-sensor_R = RGBsensor('RGBSENSOR_RIGHT')
-sensor_R.load_config()
-sensor_R.print_config()
-sensor_R.initiate_sensor()
+#sensor_R = RGBsensor('RGBSENSOR_RIGHT')
+#sensor_R.load_config()
+#sensor_R.print_config()
+#sensor_R.initiate_sensor()
 
 
 try:
     while True:
         result_L = sensor_L.sense_RGB()
-        result_R = sensor_R.sense_RGB()
-        print("LEFT [r,g,b]" + str(result_L) + "RIGHT [r,g,b]" + str(result_R))
+        #result_R = sensor_R.sense_RGB()
+        print(result_L)
+        #print("LEFT [r,g,b]" + str(result_L) + "RIGHT [r,g,b]" + str(result_R))
         #time.sleep(0.2)
 
 except KeyboardInterrupt:
