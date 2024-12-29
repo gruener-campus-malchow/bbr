bccea7f74f8af3e0bfde8e0b4cf5e6b1d9fe8f49
Alexander Baldauf <baldauf@gruener-campus-malchow.de>
2024-12-29 17:40:57 +0100

    tag3.3

--- src/RGBsensor.py
+++ src/RGBsensor.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*- 
-
 # this code based on https://www.electronicshub.org/raspberry-pi-color-sensor-tutorial/
 
 import RPi.GPIO as GPIO
 import time
 import configparser
 
-# should go into config
+#| s3 | s4 | selected diode             |
+#|----|----|----------------------------|
+#| L  | L  | red                        |
+#| L  | H  | blue                       |
+#| H  | L  | clear (no filter on diode) |
+#| H  | H  | green                      |
 
 class RGBsensor():
 
@@ -14,11 +18,9 @@ class RGBsensor():
     s2 = 23
     s3 = 24
     signal = 25
-    NUM_CYCLES = 10 #think, this is number of timeslots waiting for good signal from sensor
-    
+    NUM_CYCLES = 10 #think, this is number of values used for one measurement    
     def __init__(self, name):
     	self.name = name
-    
 
     def initiate_sensor(self):
         GPIO.setmode(GPIO.BCM)
@@ -28,6 +30,8 @@ class RGBsensor():
         #print("\n")
 
     def sense_RGB(self):
+    
+            # measures red
             GPIO.output(self.s2,GPIO.LOW)
             GPIO.output(self.s3,GPIO.LOW)
             time.sleep(0.3) #important?
@@ -37,7 +41,8 @@ class RGBsensor():
             duration = time.time() - start      #seconds to run for loop
             red  = self.NUM_CYCLES / duration   #in Hz
             #print("red value - ",red)
-
+            
+            # measures blue
             GPIO.output(self.s2,GPIO.LOW)
             GPIO.output(self.s3,GPIO.HIGH)
             time.sleep(0.3)
@@ -47,7 +52,8 @@ class RGBsensor():
             duration = time.time() - start
             blue = self.NUM_CYCLES / duration
             #print("blue value - ",blue)
-
+            
+            # measures green
             GPIO.output(self.s2,GPIO.HIGH)
             GPIO.output(self.s3,GPIO.HIGH)
             time.sleep(0.3)#important?
@@ -64,8 +70,13 @@ class RGBsensor():
     def load_config(self):
         config = configparser.ConfigParser()
         config.read('config.ini')
-        #self.pin = int(config[self.name]['pin'])
+        self.s2 = int(config[self.name]['s2_pin'])
+        self.s3 = int(config[self.name]['s3_pin'])
+        self.signal = int(config[self.name]['signal_pin'])
+        self.NUM_CYCLES = int(config[self.name]['NUM_CYCLES'])
         
     def print_config(self):
-    
-        print("comming soon")
+        print("pin of the s2-diode-selektor: " + self.s2)
+        print("pin of the s3-diode-selektor: " + self.s3)
+        print("pin of the sensor signal: " + self.signal)
+        print("Number of cycles used for each measurement: " + self.NUM_CYCLES)
--- src/test_sensor.py
+++ src/test_sensor.py
@@ -2,7 +2,7 @@ from RGBsensor import RGBsensor
 import time
 import RPi.GPIO as GPIO
 
-sensor = RGBsensor('SENSOR_LEFT')
+sensor = RGBsensor('RGBSENSOR_LEFT')
 sensor.load_config()
 sensor.print_config()
 sensor.initiate_sensor()
@@ -11,7 +11,7 @@ try:
     while True:
         result = sensor.sense_RGB()
         print(result)
-        time.sleep(0.2)
+        #time.sleep(0.2)
 
 except KeyboardInterrupt:
     #servo.stop()
