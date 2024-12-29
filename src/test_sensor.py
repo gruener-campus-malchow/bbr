5dcf6f4881cb0c567d96aa34925c976a65e1d5fe
Alexander Baldauf <baldauf@gruener-campus-malchow.de>
2024-12-29 20:42:30 +0100

    tag3.4

--- src/test_sensor.py
+++ src/test_sensor.py
@@ -2,22 +2,22 @@ from RGBsensor import RGBsensor
 import time
 import RPi.GPIO as GPIO
 
-sensor_L = RGBsensor('RGBSENSOR_LEFT')
-sensor_L.load_config()
-sensor_L.print_config()
-sensor_L.initiate_sensor()
+#sensor_L = RGBsensor('RGBSENSOR_LEFT')
+#sensor_L.load_config()
+#sensor_L.print_config()
+#sensor_L.initiate_sensor()
 
-#sensor_R = RGBsensor('RGBSENSOR_RIGHT')
-#sensor_R.load_config()
-#sensor_R.print_config()
-#sensor_R.initiate_sensor()
+sensor_R = RGBsensor('RGBSENSOR_RIGHT')
+sensor_R.load_config()
+sensor_R.print_config()
+sensor_R.initiate_sensor()
 
 
 try:
     while True:
-        result_L = sensor_L.sense_RGB()
-        #result_R = sensor_R.sense_RGB()
-        print(result_L)
+        #result_L = sensor_L.sense_RGB()
+        result_R = sensor_R.sense_RGB()
+        print(result_R)
         #print("LEFT [r,g,b]" + str(result_L) + "RIGHT [r,g,b]" + str(result_R))
         time.sleep(0.2)
 
